43df89f73b592771485ec4a7e8aa52475db84e3d
Antoni Kania <antek.kania@wp.pl>
2025-06-09 12:56:38 +0200

    fix: fix unit tests

--- components/__tests__/ThemedText-test.tsx
+++ components/__tests__/ThemedText-test.tsx
@@ -3,6 +3,10 @@ import renderer from 'react-test-renderer';
 
 import { ThemedText } from '../ThemedText';
 
+jest.mock('../../hooks/useColorScheme', () => ({
+  useColorScheme: jest.fn(() => 'light'),
+}));
+
 it(`renders correctly`, () => {
   const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
 
---
+++ jest.setup.js
@@ -0,0 +1,6 @@
+jest.mock('expo-location', () => ({
+  requestForegroundPermissionsAsync: jest.fn(() => Promise.resolve({ status: 'granted' })),
+  getCurrentPositionAsync: jest.fn(() => Promise.resolve({
+    coords: { latitude: 0, longitude: 0 },
+  })),
+}));
